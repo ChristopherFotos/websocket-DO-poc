3977749e9098f09c795bfa8629a544452d160885
Christopher Fotos <christopherfotos@Christophers-MacBook-Pro.local>
2024-07-02 22:41:39 -0400

    combine server files

--- index.js
+++ index.js
@@ -10,4 +10,14 @@ app.get("/home", (req, res) => {
   res.send("hello index");
 });
 
-app.listen(8080, () => console.log("Server started on port" + "8080"));
+app.listen(8080, () => console.log("Server started on port " + "8080"));
+
+const socket = express();
+socket.use(cors());
+socket.use(express.static(resolve("../client")));
+
+socket.get("/home", (req, res) => {
+  res.send("hello websocket");
+});
+
+socket.listen(8081, () => console.log("Server started on port " + "8081"));
