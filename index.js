bc8f68819048fa0e7aae8b905e6384e5a242a805
Christopher Fotos <christopherfotos@Christophers-MacBook-Pro.local>
2024-07-03 20:49:29 -0400

    add test code from repo i found

--- index.js
+++ index.js
@@ -1,60 +1,31 @@
-// const express = require("express");
-// const cors = require("cors");
-// resolve = require("path").resolve;
+const express = require("express");
+const path = require("path");
+const { createServer } = require("http");
 
-// const app = express();
-// app.use(cors());
-// app.use(express.static(resolve("./public")));
+const WebSocket = require("ws");
 
-// app.get("/home", (req, res) => {
-//   res.send("hello index");
-// });
+const app = express();
+app.use(express.static(path.join(__dirname, "/public")));
 
-// app.listen(8080, () => console.log("Server started on port " + "8080"));
+const server = createServer(app);
+const wss = new WebSocket.Server({ server });
 
-// SOCKET
+wss.on("connection", function (ws) {
+  const id = setInterval(function () {
+    ws.send(JSON.stringify(process.memoryUsage()), function () {
+      //
+      // Ignoring errors.
+      //
+    });
+  }, 100);
+  console.log("started client interval");
 
-let number = 0;
-
-const http = require("https");
-
-const socketServer = http.createServer((req, res) => {
-  const headers = {
-    "Access-Control-Allow-Origin": "*" /* @dev First, read about security */,
-    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-    "Access-Control-Max-Age": 2592000, // 30 days
-    /** add other headers as per requirement */
-  };
-
-  if (req.method === "OPTIONS") {
-    res.writeHead(204, headers);
-    res.end();
-    return;
-  }
-
-  if (["GET", "POST"].indexOf(req.method) > -1) {
-    res.writeHead(200, headers);
-    res.end("Hello World");
-    return;
-  }
-
-  res.writeHead(405, headers);
-  res.end(`${req.method} is not allowed for the request.`);
-});
-
-const io = require("socket.io")(socketServer, {
-  cors: { origin: "*" },
-});
-
-io.on("connection", (socket) => {
-  console.log("Someone has connected");
-  io.emit("connected", "someone connected");
-
-  socket.on("up", () => {
-    console.log("up");
-    number++;
-    io.emit("update", number);
+  ws.on("close", function () {
+    console.log("stopping client interval");
+    clearInterval(id);
   });
 });
 
-socketServer.listen(8080, () => console.log("socket listening on 8080"));
+server.listen(8080, function () {
+  console.log("Listening on http://0.0.0.0:8080");
+});
