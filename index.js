82a8ef73cee84afefccf8192d85a461b9fc61392
Christopher Fotos <christopherfotos@Christophers-MacBook-Pro.local>
2024-07-04 21:46:15 -0400

    revert to example code for debugging

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// const express = require("express");
+// const path = require("path");
+// const { createServer } = require("http");
+
+// const WebSocket = require("ws");
+
+// const app = express();
+// app.use(express.static(path.join(__dirname, "/public")));
+
+// const server = createServer(app);
+// const wss = new WebSocket.Server({ server });
+
+// let number = 0;
+
+// wss.on("connection", function (ws) {
+//   console.log("connected");
+
+//   ws.on("message", (event) => {
+//     console.log("stopping client interval");
+//     if (event.type === "up") {
+//       number++;
+//     }
+
+//     ws.send({ type: "update", number });
+//   });
+// });
+
+// server.listen(8080, function () {
+//   console.log("Listening on http://0.0.0.0:8080");
+// });
+
+///////////////////
+"use strict";
+
 const express = require("express");
 const path = require("path");
 const { createServer } = require("http");
@@ -10,18 +44,19 @@ app.use(express.static(path.join(__dirname, "/public")));
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
 
-let number = 0;
-
 wss.on("connection", function (ws) {
-  console.log("connected");
+  const id = setInterval(function () {
+    ws.send(JSON.stringify(process.memoryUsage()), function () {
+      //
+      // Ignore errors.
+      //
+    });
+  }, 100);
+  console.log("started client interval");
 
-  ws.on("message", (event) => {
+  ws.on("close", function () {
     console.log("stopping client interval");
-    if (event.type === "up") {
-      number++;
-    }
-
-    ws.send({ type: "update", number });
+    clearInterval(id);
   });
 });
 
--- public/client.js
+++ public/client.js
@@ -1,37 +1,38 @@
-console.log("aloha");
+// console.log("aloha");
 
-const protocol = window.location.protocol.includes("https") ? "wss" : "ws";
-const ws = new WebSocket(`${protocol}://${location.host}`);
+// const protocol = window.location.protocol.includes("https") ? "wss" : "ws";
+// const ws = new WebSocket(`${protocol}://${location.host}`);
 
-const btn = document.getElementById("count-btn");
-const count = document.getElementById("count");
-btn.addEventListener("click", () => {
-  ws.send("up");
-});
+// const btn = document.getElementById("count-btn");
+// const count = document.getElementById("count");
 
-ws.onmessage = (event) => {
-  console.log(event.data);
-  if (event.data.type === "update") count.innerText = event.data.number;
-};
+// btn.addEventListener("click", () => {
+//   ws.send("up");
+// });
+
+// ws.onmessage = (event) => {
+//   console.log(event.data);
+//   if (event.data.type === "update") count.innerText = event.data.number;
+// };
 
 /////////////
 
-// (function () {
-//   const rss = document.getElementById("rss");
-//   const heapTotal = document.getElementById("heapTotal");
-//   const heapUsed = document.getElementById("heapUsed");
-//   const external = document.getElementById("external");
+(function () {
+  const rss = document.getElementById("rss");
+  const heapTotal = document.getElementById("heapTotal");
+  const heapUsed = document.getElementById("heapUsed");
+  const external = document.getElementById("external");
 
-//   // will not use tls if the connection is not made over https
-//   const protocol = window.location.protocol.includes("https") ? "wss" : "ws";
-//   const ws = new WebSocket(`${protocol}://${location.host}`);
+  // will not use tls if the connection is not made over https
+  const protocol = window.location.protocol.includes("https") ? "wss" : "ws";
+  const ws = new WebSocket(`${protocol}://${location.host}`);
 
-//   ws.onmessage = function (event) {
-//     const data = JSON.parse(event.data);
+  ws.onmessage = function (event) {
+    const data = JSON.parse(event.data);
 
-//     rss.textContent = data.rss;
-//     heapTotal.textContent = data.heapTotal;
-//     heapUsed.textContent = data.heapUsed;
-//     external.textContent = data.external;
-//   };
-// })();
+    rss.textContent = data.rss;
+    heapTotal.textContent = data.heapTotal;
+    heapUsed.textContent = data.heapUsed;
+    external.textContent = data.external;
+  };
+})();
