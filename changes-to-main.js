6bbed0f222bdba68d70ba01c16b1cb9900616c02
Christopher Fotos <christopherfotos@Christophers-MacBook-Pro.local>
2024-11-13 00:45:40 -0500

    testing webhook

--- changes-to-main.js
+++ changes-to-main.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 const crypto = require("crypto");
 
-// comment to test webhook 4
+// comment to test webhook 5
 
 const verifySignature = (req, secret) => {
   const signature = `sha256=${crypto
